85f6eb7888ca37d86423a6af2e0425dd71879b3c
Julian Sauer <julian_sauer@gmx.net>
2020-02-21 11:35:57 +0100

    Update clone script

--- Backup/backup-rclone.sh
+++ Backup/backup-rclone.sh
@@ -3,11 +3,12 @@
 timestamp=$(date +%Y%m%d-%H%M%S)
 filename=${1}-${timestamp}
 
-echo "Backing up ${directory}${1} as ${filename} using rclone"
+echo "Backing up ${2}${1} as ${filename} using rclone"
 
 if [[ -n "${2}" ]];
 then
-	cp -r ${2}${1} ./
+        echo "Copying ${2}${1} to ./${1}"
+	cp -r ${2}${1} ./${1}
 fi
 
 tar cvf ${filename}.tar ${1}
@@ -15,6 +16,7 @@ mv ${filename}.tar ./volumes/rclone/drive/
 
 if [[ -n "${2}" ]];
 then
+        echo "Deleting ./${1}"
 	rm -rf ${1}
 fi
 
